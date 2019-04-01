b4e16c3b16b5ae975549c7bb58908459e58056c2
Rafael Câmara <rafaelcamaram@gmail.com>
2019-03-31 22:53:13 -0300

    Fixing URL_HOST to production and rmeoving unnecessary dependencies

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 const CONSTANTS = {
-  URL_HOST: 'http://localhost:3030'
-  // URL_HOST: 'https://tp-otimizacao-back.herokuapp.com'
+  // URL_HOST: 'http://localhost:3030'
+  URL_HOST: 'https://tp-otimizacao-back.herokuapp.com'
 };
 
 export default CONSTANTS;
